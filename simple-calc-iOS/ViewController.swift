e43d9e44144d060dd2032f97280efc90a0605dfb
Benny Souriyadeth <bennys@uw.edu>
2018-01-25 00:41:18 -0800

    Initial Commit

---
+++ simple-calc-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple-calc-iOS
+//
+//  Created by Benny on 1/25/18.
+//  Copyright © 2018 Benny Souriyadeth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
