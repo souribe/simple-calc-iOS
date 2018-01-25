1038d0b68acd4973c5040e4c6c66f9d6717db9e4
Benny Souriyadeth <bennys@uw.edu>
2018-01-25 00:59:03 -0800

    number button press added

--- simple-calc-iOS/ViewController.swift
+++ simple-calc-iOS/ViewController.swift
@@ -8,13 +8,77 @@
 
 import UIKit
 
+enum Operation : String {
+    case Add = "+"
+    case Subtract = "-"
+    case Divide = "/"
+    case Multiply = "*"
+    case Mod = "%"
+    case Count = "count"
+    case Avg = "avg"
+    case Fact = "fact"
+    case Null = "Null"
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //label.text = "0" // changes text to 0
     }
-
+    
+    var numbers = ""
+    var lValue = ""
+    var rvalue = ""
+    var result = ""
+    var operation : Operation = .Null
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func numbersPressed(_ sender: UIButton) {
+        numbers += "\(sender.tag)"
+        label.text = numbers
+    }
+    
+    @IBAction func dot(_ sender: UIButton) {
+        numbers += "."
+        label.text = numbers
+    }
+    
+    @IBAction func allClear(_ sender: UIButton) {
+    }
+    
+    @IBAction func equals(_ sender: UIButton) {
+    }
+    
+    @IBAction func add(_ sender: UIButton) {
+    }
+    
+    @IBAction func subtract(_ sender: UIButton) {
+    }
+    
+    @IBAction func multiply(_ sender: UIButton) {
+    }
+    
+    @IBAction func divide(_ sender: UIButton) {
+    }
+    
+    @IBAction func mod(_ sender: UIButton) {
+    }
+    
+    @IBAction func count(_ sender: UIButton) {
+    }
+    
+    @IBAction func avg(_ sender: UIButton) {
+    }
+    
+    @IBAction func fact(_ sender: UIButton) {
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
